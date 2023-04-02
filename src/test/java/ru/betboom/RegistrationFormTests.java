39c1de23a19b08b6241da114a7940e168174de7a
Ilya Tyunin <tomslavg96@gmail.com>
2023-04-02 12:57:26 +0300

    add structure of project

---
+++ src/test/java/ru/betboom/RegistrationFormTests.java
@@ -0,0 +1,19 @@
+package ru.betboom;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class RegistrationFormTests extends TestBase {
+    @Test
+    void fillFormTest() {
+        open("/automation-practice-form");
+
+        $("#firstName").setValue("Ilya");
+        $("#lastName").setValue("Tyunin");
+        $("#userEmail").setValue("my@bb.org");
+        $("#userEmail").setValue("my@bb.org");
+        $("#gender-radio-1").click();
+    }
+}
---
+++ src/test/java/ru/betboom/TestBase.java
@@ -0,0 +1,13 @@
+package ru.betboom;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+public class TestBase {
+    @BeforeAll
+    static void firstConfigure() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.holdBrowserOpen = true;
+    }
+}
