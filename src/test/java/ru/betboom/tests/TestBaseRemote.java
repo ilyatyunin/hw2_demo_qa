bd9e111abd99c2d99bbccb28efecaa5540394b14
Ilya Tyunin <tomslavg96@gmail.com>
2023-06-22 20:49:42 +0300

    added attachs - screen, pagesource, console logs

--- src/test/java/ru/betboom/tests/TestBaseRemote.java
+++ src/test/java/ru/betboom/tests/TestBaseRemote.java
@@ -3,9 +3,11 @@ package ru.betboom.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.betboom.helpers.Attach;
 
 import java.util.Map;
 
@@ -28,5 +30,12 @@ public class TestBaseRemote {
     @BeforeEach
     void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last Screeshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
     }
 }
