8c0f2e8d13ef339ae4543b1d8777d7f38476c83f
Ilya Tyunin <tomslavg96@gmail.com>
2023-06-22 20:32:15 +0300

    added remote with selenoid

--- src/test/java/ru/betboom/tests/TestBaseRemote.java
+++ src/test/java/ru/betboom/tests/TestBaseRemote.java
@@ -1,7 +1,10 @@
 package ru.betboom.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
@@ -20,5 +23,10 @@ public class TestBaseRemote {
                 "enableVideo", true
         ));
         Configuration.browserCapabilities = capabilities;
+    }
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 }
